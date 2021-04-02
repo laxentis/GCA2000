cf461564cd3a503bc724467fc1018a9cc7f8606e
laxentis <laxentis@gmail.com>
2021-04-02 15:18:49 +0200

    New Label class

---
+++ GCA2000/CGCALabel.cpp
@@ -0,0 +1,69 @@
+#include "pch.h"
+#include "CGCALabel.h"
+#define _USE_MATH_DEFINES
+#include <functional>
+#include <math.h>
+
+CGCALabel::CGCALabel(EuroScopePlugIn::CRadarTarget track, EuroScopePlugIn::CPosition threshold, int thresholdAltitude,
+                     int heading,
+                     double glideSlope)
+{
+	this->RadarTrackPosition = track.GetPosition().GetPosition();
+	this->Callsign = track.GetCallsign();
+	this->Altitude = track.GetPosition().GetPressureAltitude();
+	this->RunwayThreshold = threshold;
+	this->ThresholdAltitude = thresholdAltitude;
+	this->FinalApproachHeading = heading;
+	this->GlideslopeAngle = glideSlope;
+	this->TrackDeviation = 0;
+	this->TrackDistance = 0;
+	this->GlidepathDeviation = 0;
+	CalculateTrackDistance();
+	CalculateTrackDeviation();
+	CalculateGlidepathDeviation();
+}
+
+void CGCALabel::CalculateTrackDistance()
+{
+	auto trackAngle = this->RunwayThreshold.DirectionTo(this->RadarTrackPosition);
+	trackAngle = trackAngle * M_PI / 180;
+	const auto straightDistance = this->RunwayThreshold.DistanceTo(this->RadarTrackPosition);
+	this->TrackDistance = cos(trackAngle) * straightDistance;
+}
+
+void CGCALabel::CalculateTrackDeviation()
+{
+	auto trackAngle = this->RunwayThreshold.DirectionTo(this->RadarTrackPosition);
+	trackAngle = trackAngle * M_PI / 180;
+	const auto straightDistance = this->RunwayThreshold.DistanceTo(this->RadarTrackPosition);
+	this->TrackDeviation = sin(trackAngle) * straightDistance;
+}
+
+double CGCALabel::GetPerfectAltitudeForPresentDistance() const
+{
+	double altitude = ThresholdAltitude;
+	const auto glideSlopeRadians = this->GlideslopeAngle * M_PI / 180;
+	altitude += tan(glideSlopeRadians) * this->TrackDistance;
+	return altitude;
+}
+
+void CGCALabel::CalculateGlidepathDeviation()
+{
+	const auto perfectAltitude = GetPerfectAltitudeForPresentDistance();
+	this->GlidepathDeviation = this->Altitude - perfectAltitude;
+}
+
+double CGCALabel::GetTrackDistance() const
+{
+	return this->TrackDistance;
+}
+
+double CGCALabel::GetTrackDeviation() const
+{
+	return this->TrackDeviation;
+}
+
+double CGCALabel::GetGlidepathDeviation() const
+{
+	return this->GlidepathDeviation;
+}
---
+++ GCA2000/CGCALabel.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <string>
+#include <EuroScopePlugIn.h>
+class CGCALabel
+{
+public:
+	CGCALabel(EuroScopePlugIn::CRadarTarget track, EuroScopePlugIn::CPosition threshold, int thresholdAltitude, int heading, double glideSlope);
+	double GetTrackDistance() const;
+	double GetTrackDeviation() const;
+	auto GetGlidepathDeviation() const -> double;
+
+private:
+	// Calculated or retrieved parameters
+	std::string Callsign;
+	int Altitude;
+	double TrackDeviation;
+	double GlidepathDeviation;
+	double TrackDistance;
+
+	// Passed and stored parameters
+	int FinalApproachHeading;
+	double GlideslopeAngle;
+	EuroScopePlugIn::CPosition RadarTrackPosition;
+	EuroScopePlugIn::CPosition RunwayThreshold;
+	int ThresholdAltitude;
+
+	// Calculation of label elements
+	void CalculateTrackDistance();
+	void CalculateTrackDeviation();
+	void CalculateGlidepathDeviation();
+
+	// Helper calculators
+	double GetPerfectAltitudeForPresentDistance() const;
+};
+
